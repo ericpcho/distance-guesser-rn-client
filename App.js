3283c81e76789eca4848cdb871f3c4caa86d3234
ericpcho <ericpcho@gmail.com>
2017-11-25 10:53:13 -0800

    current cors error version

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, ListItem, Button } from 'react-native-elements'
+import {API_BASE_URL} from './config'
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      view: 'home',
+      cityData: []
+    }
+  }
+
+  fetchCities() {
+
+    
+    fetch(`${API_BASE_URL}/api/cities`)
+    .then(res => {
+      console.log(res);
+      return res.json()
+    })
+      .then((cities) => {console.log(cities)})
+      .catch(err => console.log(err))
+  }
+
+  componentDidMount() {
+    this.fetchCities();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-      
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>U.S. Geography Game</Text>
       </View>
     );
   }
@@ -18,6 +43,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 20,
   },
 });
